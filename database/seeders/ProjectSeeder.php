b23c22c89ccafa9795a5c7a2959e203ca23b4a4e
EliaPennoni <elia.pennoni1991@gmail.com>
2024-10-30 23:31:14 +0100

    creato associazione id sulla colonna project

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\project;
+use App\Models\Type;
 use Faker\Factory as Faker;
 use Illuminate\Support\Str;
 
@@ -21,12 +22,15 @@ class ProjectSeeder extends Seeder
         for ($i = 0; $i < 10; $i++) {
             $title = $faker->sentence(3);
 
+            $randomType = Type::inRandomOrder()->first();
+
             Project::create([
                 'title' => $title,
                 'slug' => Str::slug($title),
                 'price' => $faker->randomFloat(2, 1, 999),
                 'description' => $faker->paragraph(),
                 'image' => $faker->imageUrl(640, 480, 'projects', true),
+                'type_id' => $randomType->id,
             ]);
         }
     }
